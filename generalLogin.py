7ee1483f0a3fb98ad5e70187cae9969e82ce72c0
Huang Runping <pingrunhuang@gmail.com>
2018-02-07 15:02:36 +0800

    added wifi login

--- generalLogin.py
+++ generalLogin.py
@@ -12,7 +12,6 @@ headers = {
    "Accept-Encoding": "gzip, deflate, br",
    "Accept-Language": "en-US, en; q=0.8, zh-Hans-CN; q=0.7, zh-Hans; q=0.5, fr-FR; q=0.3, fr; q=0.2",
    "Connection": "Keep-Alive",
-   "Cookie": "sjsinfo.session.id=f7bca64a800449fd91f52ba2157d1199",
    "Host": "oa.shijinshi.cn",
    "Referer": "https://oa.shijinshi.cn/sjsinfo/main",
    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
@@ -25,13 +24,15 @@ payload = {
 
 session = requests.session()
 response = session.post(url=homeurl, data=payload, headers=headers)
-print("response code from server is ", response.status_code)
-soup = BeautifulSoup(response.content, 'html.parser')
-links = soup.find_all('a')
-for link in links:
-    print(link)
-print("Before login: ", soup.find('a'))
-headers['Referer'] = 'http://oa.shijinshi.cn/sjsinfo/main/oa/workClockInRecord/clockInOrOut'
-response = session.post(url="http://oa.shijinshi.cn/sjsinfo/main/login", data=payload, headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
-print("After login:", soup.find('a'))
+if response.status_code==200:
+    print("Successfully logged in...")
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print("Before sign in: ", soup.find('a'))
+    headers['Referer'] = 'http://oa.shijinshi.cn/sjsinfo/main'
+    headers['Cookie'] = "sjsinfo.session.id="+session.cookies.get('sjsinfo.session.id')
+    print(headers)
+    response = session.post(url="http://oa.shijinshi.cn/sjsinfo/main/oa/workClockInRecord/clockInOrOut", data=payload, headers=headers)
+    print("After sign in:", response.content)
+else:
+    print('Failed login')
+
---
+++ loginWIFI.py
@@ -0,0 +1,43 @@
+import requests
+
+'''
+This script is used for automatically log in to our wifi system
+'''
+
+home_url = 'http://192.168.14.118/portal/'
+login_url = 'http://192.168.14.118/portal/logon.cgi'
+
+headers = {
+    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding":"gzip, deflate",
+    "Accept-Language":"en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7",
+    "Cache-Control":"max-age=0",
+    "Connection":"keep-alive",
+    "Host":"192.168.14.118",
+    "Referer":home_url,
+    "Upgrade-Insecure-Requests":"1",
+    "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36"
+}
+
+# this is part is really something that should be change according to different website 
+# checkout the form data part and include every part in that block to this field
+payload = {
+    "PtUser": input('Please enter your username: '),
+    "PtPwd": input('Please enter your password: '),
+    "PtButton": "Logon",
+}
+
+session = requests.Session()
+
+response = None
+try:
+    response = session.get(url=home_url, data=payload, headers=headers)
+except requests.exceptions.ConnectionError as error:
+    print(error)
+
+headers['Origin']='http://192.168.14.118'
+headers['Referer']='http://192.168.14.118/portal/'
+
+response = session.post(url=login_url, data=payload, headers=headers)
+print('You have logged in: ', response.status_code)
+
